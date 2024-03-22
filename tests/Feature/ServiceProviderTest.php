600277092861ac2eca6ffd7ac21ea74e6e1e007d
Philipp Daun <post@philippdaun.net>
2024-03-22 14:55:57 +0100

    Test service provider bindings

--- tests/Feature/ServiceProviderTest.php
+++ tests/Feature/ServiceProviderTest.php
@@ -1,9 +1,15 @@
 <?php
 
+use Daun\StatamicPlaceholders\ServiceProvider;
 use Daun\StatamicPlaceholders\Services\ImageManager;
 use Daun\StatamicPlaceholders\Services\PlaceholderProviders;
 use Daun\StatamicPlaceholders\Services\PlaceholderService;
 use Intervention\Image\ImageManager as InterventionImageManager;
+
+test('provides services', function () {
+    $provider = new ServiceProvider($this->app);
+    expect($provider->provides())->toBeArray()->not->toBeEmpty();
+});
 
 test('binds provider service', function () {
     expect($this->app[PlaceholderProviders::class])->toBeInstanceOf(PlaceholderProviders::class);
